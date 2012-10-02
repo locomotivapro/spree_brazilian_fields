b03e7abd07ae62827766a88822e547d9725103d9
Denis <denisantoniazzi@Deniss-MacBook-Pro.local>
2012-10-02 13:41:48 -0300

    add format_cnpj method

--- app/helpers/spree/admin/brazilian_fields_helper.rb
+++ app/helpers/spree/admin/brazilian_fields_helper.rb
@@ -7,6 +7,11 @@ module Spree
         "#{cpf1}.#{cpf2}.#{cpf3}-#{cpf4}"
       end
       
+      def formated_cnpj(cnpj)
+        cnpj1, cnpj2, cnpj3, cnpj4, cnpj5 = cnpj[0...2], cnpj[2...5], cnpj[5...8], cnpj[8...12], cnpj[12...14]
+        "#{cnpj1}.#{cnpj2}.#{cnpj3}/#{cnpj4}-#{cnpj5}"
+      end
+      
     end
   end
 end
--- spec/helpers/spree/admin/brazilian_fields_helper_spec.rb
+++ spec/helpers/spree/admin/brazilian_fields_helper_spec.rb
@@ -10,4 +10,9 @@ describe Spree::Admin::BrazilianFieldsHelper do
     formated_cpf(cpf2).should eq('123.456.789-00')
   end
   
+  it "returns CNPJ in format XX.XXX.XXX/XXXX-XX" do
+    cnpj = '12345789000100'
+    formated_cnpj(cnpj).should eq('12.345.789/0001-00')
+  end
+  
 end

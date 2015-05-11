6b2db4acacaba149b69fbe0b3c0d17680fa80cb2
Denis Tierno <de.tierno@gmail.com>
2015-05-11 16:48:25 -0300

    Reorganized assets path and includes

--- lib/generators/spree_brazilian_fields/install/install_generator.rb
+++ lib/generators/spree_brazilian_fields/install/install_generator.rb
@@ -1,17 +1,17 @@
 module SpreeBrazilianFields
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      
+
       # Dont use this generators because we include all stylesheets and javascripts directly on file to avoid unecessario loads
-      # def add_javascripts
-      #   append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_brazilian_fields\n"
-      #   append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_brazilian_fields\n"
-      # end
-      # 
-      # def add_stylesheets
-      #   inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_brazilian_fields\n", :before => /\*\//, :verbose => true
-      #   inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_brazilian_fields\n", :before => /\*\//, :verbose => true
-      # end
+       def add_javascripts
+         append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_brazilian_fields\n"
+         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_brazilian_fields\n"
+       end
+
+       def add_stylesheets
+         inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_brazilian_fields\n", :before => /\*\//, :verbose => true
+         inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_brazilian_fields\n", :before => /\*\//, :verbose => true
+       end
 
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_brazilian_fields'
